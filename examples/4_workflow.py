43d426f28925677b50856a31d83de9e1caf9db97
taf <dev@taf.codes>
2025-02-13 16:32:19 -0500

    examples/4: cleanup

--- examples/4_workflow.py
+++ examples/4_workflow.py
@@ -10,10 +10,10 @@ find_urls_instructions = \
     "Find me the links to each detail page for a bicycle for sale."
 
 bike_for_sale_template = {
-    "full_description": "The seller's entire textual description.",
-    "frame_size": "The size of the bicyle frame.",
-    "price": "The price the seller is asking for the bike.",
-    "MSRP": "The original MSRP price of the bike as given by this listing, or (not found) if there is not one provided."
+    "full_description": "Find the seller's entire textual description.",
+    "frame_size": "Find the size of the bicyle frame.",
+    "price": "What price is the seller asking for the bike?",
+    "MSRP": "What is the original MSRP price of the bike as given by this listing, or (not found) if there is not one provided."
 }
 
 filter_instructions = "Exclude any listings where the description includes phrases such as 'no tire kickers' or 'i know what i have' or 'serious buyers only' or 'no lowballers' or anything else that suggests an unwillingness to negotiate."
@@ -26,7 +26,6 @@ workflow = (
     .filter(filter_instructions)
 )
 
-workflow_id = fox.register_workflow(workflow)
-job_id = fox.run_workflow(workflow_id=workflow_id)
+job_id = fox.run_workflow(workflow=workflow)
 result_items = fox.await_job_completion(job_id)
 pprint(result_items)
