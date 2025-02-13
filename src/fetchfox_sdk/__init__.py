0728cb3714ee3156d15cddbaa93524fe8c293e20
taf <dev@taf.codes>
2025-02-13 16:42:50 -0500

    call it 0.0.2

--- src/fetchfox_sdk/__init__.py
+++ src/fetchfox_sdk/__init__.py
@@ -6,5 +6,5 @@ logger = logging.getLogger("fetchfox")
 logger.setLevel(logging.WARNING)
 logger.addHandler(logging.NullHandler())
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __all__ = ["FetchFoxSDK","Workflow"]
