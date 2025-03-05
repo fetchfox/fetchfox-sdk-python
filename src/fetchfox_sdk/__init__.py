f48c82c09f25020517378d57480fd3929d6aa6a4
taf <dev@taf.codes>
2025-03-04 22:24:50 -0500

    bump version

--- src/fetchfox_sdk/__init__.py
+++ src/fetchfox_sdk/__init__.py
@@ -5,5 +5,5 @@ logger = logging.getLogger("fetchfox")
 logger.setLevel(logging.WARNING)
 logger.addHandler(logging.NullHandler())
 
-__version__ =  "0.1.0"
+__version__ =  "0.2.0"
 __all__ = ["FetchFox"]
